1e0c1d595c2853fbcab9ea68b26be9bd511a5857
R1nge <r1ngegamedev@gmail.com>
2023-03-11 19:30:20 +0300

    ref: robot controller

---
+++ Unity/Assets/Scripts/MyController.cs
@@ -0,0 +1,85 @@
+﻿using Unity.Robotics.UrdfImporter.Control;
+using UnityEngine;
+
+public class MyController : MonoBehaviour
+{
+    private ArticulationBody[] _articulationChain;
+    private int _previousIndex;
+    private string _selectedJoint;
+    private int _selectedIndex;
+
+    public ControlType control = ControlType.PositionControl;
+    public float stiffness;
+    public float damping;
+    public float forceLimit;
+    public float speed = 5f; // Units: degree/s
+
+    private void Start()
+    {
+        _previousIndex = _selectedIndex = 1;
+        gameObject.AddComponent<FKRobot>();
+        _articulationChain = GetComponentsInChildren<ArticulationBody>();
+        int dynamicValue = 10;
+        foreach (ArticulationBody joint in _articulationChain)
+        {
+            joint.gameObject.AddComponent<MyJointController>();
+            joint.jointFriction = dynamicValue;
+            joint.angularDamping = dynamicValue;
+            ArticulationDrive currentDrive = joint.xDrive;
+            currentDrive.forceLimit = forceLimit;
+            joint.xDrive = currentDrive;
+        }
+    }
+
+    void SetSelectedJointIndex(int index)
+    {
+        if (_articulationChain.Length > 0)
+        {
+            _selectedIndex = (index + _articulationChain.Length) % _articulationChain.Length;
+        }
+    }
+
+    void Update()
+    {
+        SetSelectedJointIndex(_selectedIndex); // to make sure it is in the valid range
+        UpdateDirection(_selectedIndex);
+    }
+
+
+    /// <summary>
+    /// Sets the direction of movement of the joint on every update
+    /// </summary>
+    /// <param name="jointIndex">Index of the link selected in the Articulation Chain</param>
+    private void UpdateDirection(int jointIndex)
+    {
+        if (jointIndex < 0 || jointIndex >= _articulationChain.Length)
+        {
+            return;
+        }
+
+        MyJointController current = _articulationChain[jointIndex].GetComponent<MyJointController>();
+        if (_previousIndex != jointIndex)
+        {
+            MyJointController previous = _articulationChain[_previousIndex].GetComponent<MyJointController>();
+            previous.SetRotationDirection(RotationDirection.None);
+            _previousIndex = jointIndex;
+        }
+
+        if (current.GetControlType() != control)
+        {
+            UpdateControlType(current);
+        }
+    }
+
+    public void UpdateControlType(MyJointController myJoint)
+    {
+        myJoint.SetControlType(control);
+        if (control == ControlType.PositionControl)
+        {
+            ArticulationDrive drive = myJoint.GetJoint().xDrive;
+            drive.stiffness = stiffness;
+            drive.damping = damping;
+            myJoint.GetJoint().xDrive = drive;
+        }
+    }
+}
---
+++ Unity/Assets/Scripts/MyJointController.cs
@@ -0,0 +1,91 @@
+﻿using Unity.Robotics.UrdfImporter.Control;
+using UnityEngine;
+
+public class MyJointController : MonoBehaviour
+{
+    private RotationDirection _direction;
+    private ControlType _controlType;
+    private MyController _controller;
+    private float _speed;
+    private ArticulationBody _joint;
+
+    public void SetRotationDirection(RotationDirection newDirection) => _direction = newDirection;
+
+    public ControlType GetControlType() => _controlType;
+
+    public void SetControlType(ControlType controlType) => _controlType = controlType;
+
+    public ArticulationBody GetJoint() => _joint;
+
+    private void Start()
+    {
+        _direction = 0;
+        _controller = GetComponentInParent<MyController>();
+        _joint = GetComponent<ArticulationBody>();
+        _controller.UpdateControlType(this);
+        _speed = _controller.speed;
+    }
+
+    private void FixedUpdate()
+    {
+        _speed = _controller.speed;
+
+
+        if (_joint.jointType != ArticulationJointType.FixedJoint)
+        {
+            if (_controlType == ControlType.PositionControl)
+            {
+                ArticulationDrive currentDrive = _joint.xDrive;
+                float newTargetDelta = (int)_direction * Time.fixedDeltaTime * _speed;
+
+                if (_joint.jointType == ArticulationJointType.RevoluteJoint)
+                {
+                    if (_joint.twistLock == ArticulationDofLock.LimitedMotion)
+                    {
+                        if (newTargetDelta + currentDrive.target > currentDrive.upperLimit)
+                        {
+                            currentDrive.target = currentDrive.upperLimit;
+                        }
+                        else if (newTargetDelta + currentDrive.target < currentDrive.lowerLimit)
+                        {
+                            currentDrive.target = currentDrive.lowerLimit;
+                        }
+                        else
+                        {
+                            currentDrive.target += newTargetDelta;
+                        }
+                    }
+                    else
+                    {
+                        currentDrive.target += newTargetDelta;
+                    }
+                }
+
+                else if (_joint.jointType == ArticulationJointType.PrismaticJoint)
+                {
+                    if (_joint.linearLockX == ArticulationDofLock.LimitedMotion)
+                    {
+                        if (newTargetDelta + currentDrive.target > currentDrive.upperLimit)
+                        {
+                            currentDrive.target = currentDrive.upperLimit;
+                        }
+                        else if (newTargetDelta + currentDrive.target < currentDrive.lowerLimit)
+                        {
+                            currentDrive.target = currentDrive.lowerLimit;
+                        }
+                        else
+                        {
+                            currentDrive.target += newTargetDelta;
+                        }
+                    }
+                    else
+                    {
+                        currentDrive.target += newTargetDelta;
+                    }
+                }
+
+                _joint.xDrive = currentDrive;
+            }
+        }
+    }
+}
